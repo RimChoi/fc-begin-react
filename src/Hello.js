b76d727b546760f697c4898f71020caa20629b5a
Rim Choi <sandrochia@naver.com>
2021-03-08 21:18:26 +0900

    1.6 조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-function Hello({ color, name }) {
+function Hello({ color, name, isSpecial }) {
     
-    return <h1 style={{ color, }}>안녕하세요. {name}</h1>
+    return (
+        <h1 style={{ 
+            color, 
+        }}>
+            { isSpecial ? <b>*</b> : 'X' } {/** 3항 연산자를 이용한 조건부 렌더링 */}
+            안녕하세요. {name}
+            { isSpecial && <b>*</b> } {/** && 연산자를 이용한 렌더링 */}
+        </h1>
+    );
 }
 
 Hello.defaultProps = {
